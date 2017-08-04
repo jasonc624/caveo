bdbe8536d7bbdd6f052f15c003c803ae22072ceb
Jason Cardenas <jasoncardenas@Jasons-MacBook.local>
2017-08-04 14:03:17 -0400

    added login component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import {AngularFireDatabase} from "angularfire2/database";
+import {AngularFireAuth} from "angularfire2/auth";
+import {Observable} from "rxjs/Observable";
+import * as firebase from 'firebase/app';
+import {Router} from "@angular/router";
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.sass']
+})
+export class LoginComponent implements OnInit {
+  user: Observable<firebase.User>;
+  //Models
+  username;
+  password;
+  constructor(db: AngularFireDatabase,public afAuth: AngularFireAuth, private router: Router) {
+    this.user = afAuth.authState;
+  }
+
+  ngOnInit() {
+  }
+  login() {
+    console.log('login creds', this.username, this.password);
+    this.afAuth.auth.signInWithEmailAndPassword(this.username,this.password)
+      .then(res => {
+        console.log('success you logged in!', res);
+        this.router.navigate(['']);
+      })
+      .catch(err => console.log('you fucked up buddy', err));
+  }
+
+  logout() {
+    this.afAuth.auth.signOut()
+      .then(res => console.log('you successfully signed out', res))
+      .catch(err => console.log('something went wrong', err));
+  }
+
+  register() {
+    console.log('reg', this.username, this.password);
+    this.afAuth.auth.createUserWithEmailAndPassword(this.username, this.password)
+      .then(res => console.log('success you registered',res))
+      .catch(err => console.log('something went wrong while registering', err) );
+  }
+}
